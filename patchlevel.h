e00bc1a78d03615eb8212bae5d80236a3f6332e5
Niko Tyni <ntyni@debian.org>
2011-05-15 19:35:58 +0300

    List packaged patches in patchlevel.h
    Origin: vendor
    Bug-Debian: http://bugs.debian.org/567489
    
    The list of packaged patches is in patchlevel-debian.h, which is generated
    from the debian/patches/ directory when building the package.
    
    Patch-Name: debian/patchlevel.diff

--- patchlevel.h
+++ patchlevel.h
@@ -131,6 +131,9 @@ static const char * const local_patches[] = {
 	,"uncommitted-changes"
 #endif
 	PERL_GIT_UNPUSHED_COMMITS    	/* do not remove this line */
+#ifdef DEBIAN
+#include "patchlevel-debian.h"
+#endif
 	,NULL
 };
 
