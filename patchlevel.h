ff42d90401abfbc0c3b8308809d90fd4932307ff
Niko Tyni <ntyni@debian.org>
2013-03-05 21:50:45 +0200

    [SECURITY] CVE-2013-1667: fix a rehashing DoS opportunity against code that uses arbitrary user input as hash keys. (Closes: #702296)

--- patchlevel.h
+++ patchlevel.h
@@ -190,6 +190,7 @@ static const char * const local_patches[] = {
 	,"DEBPKG:fixes/CVE-2012-5526 - [PATCH 1/4] CR escaping for P3P header"
 	,"DEBPKG:fixes/storable-security-warning - [PATCH] add a note about security concerns in Storable"
 	,"DEBPKG:fixes/maketext-code-execution - Fix misparsing of maketext strings."
+	,"DEBPKG:fixes/CVE-2013-1667 - [PATCH] Prevent premature hsplit() calls, and only trigger REHASH"
 	,"DEBPKG:patchlevel - http://bugs.debian.org/567489 List packaged patches for 5.10.1-17squeeze5 in patchlevel.h"
 	,NULL
 };
