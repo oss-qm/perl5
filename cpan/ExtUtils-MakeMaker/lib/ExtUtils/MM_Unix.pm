188bd0759e64675ba80818dabf3ed1c3ac214fb5
Niko Tyni <ntyni@debian.org>
2014-08-24 12:56:11 -0700

    Pass LD settings through to subdirectories
    [rt.cpan.org #28632] fixed most issues with passing LD down to
    subdirectory Makefile.PL files. However, there are some distributions
    (including Wx, Par::Packer, and Verilog-Perl) that explicitly set
    LD. Those still can't be overridden from the command line. Adding LD to
    the PASTHRU list fixes this.
    
    Bug-Debian: https://bugs.debian.org/758471
    Patch-Name: debian/makemaker-pasthru.diff

--- cpan/ExtUtils-MakeMaker/lib/ExtUtils/MM_Unix.pm
+++ cpan/ExtUtils-MakeMaker/lib/ExtUtils/MM_Unix.pm
@@ -2745,7 +2745,7 @@ sub pasthru {
     my($sep) = $Is{VMS} ? ',' : '';
     $sep .= "\\\n\t";
 
-    foreach my $key (qw(LIB LIBPERL_A LINKTYPE OPTIMIZE
+    foreach my $key (qw(LIB LIBPERL_A LINKTYPE OPTIMIZE LD
                      PREFIX INSTALL_BASE)
                  )
     {
